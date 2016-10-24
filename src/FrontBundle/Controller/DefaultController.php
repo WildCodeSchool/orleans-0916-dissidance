f54e8844920630ad31dbb6f8fc315d7f065f6beb
Quentin <chapellequentin@live.fr>
2016-10-24 17:55:27 +0200

    last commit quentin

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -8,10 +8,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/dissidance")
      */
     public function indexAction()
     {
         return $this->render('FrontBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/dissidance/support")
+     */
+    public function supportAction()
+    {
+        return $this->render('FrontBundle:Default:support.html.twig');
     }
 }
